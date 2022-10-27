cee98bf241d0e6c3c3f5848cdd1b7a5071e3015c
kiran7899@gmail.com <kiran7899@gmail.com>
2022-10-27 15:36:34 +0530

    Task creation

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+//Explicit task creation
+Task t = new Task(Print);
+t.Start();
+
+//Task Without start option
+Task.Factory.StartNew(() => Print());
+
+static void Print() => Console.WriteLine("Task Created");
+
