10ddd17059e22a1d804148d73df51e267372cf21
kiran7899@gmail.com <kiran7899@gmail.com>
2022-10-28 07:57:05 +0530

    Task with parameter

--- Task/Program.cs
+++ Task/Program.cs
@@ -8,5 +8,12 @@ t.Start();
 //Task Without start option
 Task.Factory.StartNew(() => Print());
 
+Task.Factory.StartNew(() => Console.Write(Count(2)));
+
 static void Print() => Console.WriteLine("Task Created");
+
+static int Count(int num)
+{
+    return num + 1;
+}
 
